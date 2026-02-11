c417eae0cfce15bf04015b0ad044507360aa69cb
VictoriaVMironova <victoria.mironova@ru.nl>
2026-02-11 16:26:40 +0100

    reset button was added

--- app.R
+++ app.R
@@ -36,8 +36,15 @@ ui <- page_sidebar(
       min = min(heart$AGE),
       max = max(heart$AGE),
       value = c(min(heart$AGE), max(heart$AGE))
+    ),
+    actionButton(
+      inputId = "reset",
+      label = "Reset",
+      icon = bsicons::bs_icon("arrow-counterclockwise")
     )
   ),
+  #------------------------------------------------
+  #tabs
   navset_tab(
     nav_panel(
       "Overview",
@@ -126,6 +133,15 @@ server <- function(input, output, session) {
       theme_minimal()
     ggplotly(p)
   })
+  
+  observeEvent(input$reset, {
+    updateSelectInput(session, "outcome", selected = "All")
+    updateSelectInput(session, "diagnosis", selected = "All")
+    updateSelectInput(session, "drg", selected = "All")
+    updateSliderInput(session, "age_range",
+                      value = c(min(heart$AGE), max(heart$AGE)))
+  })
+  
   
   output$data_table <- DT::renderDataTable({
     filtered_data()
